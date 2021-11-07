4e421a40d8e51d9d489dc84eed9fd7c1b0c01ffe
IcedFire34 <61617693+IcedFire34@users.noreply.github.com>
2021-11-07 19:43:48 +0300

    Create nick_creater.py

---
+++ Nick Creater/nick_creater.py
@@ -0,0 +1,15 @@
+import random
+letter=("a","e","i","o","u","b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","w","x","q","y","z")
+try:
+    number=int(input("Number of nicknames :"))
+    nick_lenght = int(input("Nickname lenght : "))
+except:
+    print("Please enter number !")
+    exit("Invalid Character")
+result=[]
+for i in range(0,number):
+    nick=""
+    for i in range(0,nick_lenght):
+        nick=nick+letter[int(random.random()*26)]
+    result.append(nick)
+print(result)
